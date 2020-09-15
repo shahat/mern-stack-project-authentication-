d4128567b083ca0a6c6e356cd14d08ae85ee357b
shahat <me6847570@gmail.com>
2020-09-15 03:24:23 +0200

    second commit at 15/9/2020 at 3:24 am

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-
+/* this the third layer to keep your api and secret massage secure 
+using enviromental variables */
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -15,15 +17,17 @@ app.set('view engine', 'ejs');
 /*  */
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-
-/* */
+ // add layer 2 add encription 
+ var encrypt = require('mongoose-encryption'); 
 ////////////////////////////////////
-const userSchema={
+
+const userSchema=new mongoose.Schema({
     email:String,
     password:String
+});
 
+userSchema.plugin(encrypt, { secret:process.env.SECRET , incryptedFields:["password"]});
 
-}
 const User = new mongoose.model("User",userSchema);
 
 
@@ -36,6 +40,9 @@ app.get("/login", function(req,res){
 app.get("/register",function(req,res){
         res.render("register")
         });
+app.get("/logout",function(req,res){
+            res.render("home")
+            });       
 
 app.post('/register',function(req,res){
     const newUser = new User({
@@ -44,6 +51,26 @@ app.post('/register',function(req,res){
     });
     newUser.save(err => err ? console.log(err):res.render("secrets"))
 })
+app.post("/login",function(req , res ){
+    const username = req.body.username;
+    const password = req.body.password;
+  // to check that if the user how is trying to log in is already in database or not 
+  User.findOne({ email:username},function(err,foundUser){
+if (err){
+    console.log(err)
+}else{
+    if(foundUser){
+        if(foundUser.password===password){
+            res.render("secrets")
+        }
+    }
+}
+    
+  })
+});
+
+
+
 
 
 
